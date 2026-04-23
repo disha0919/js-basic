ea572532811aa09f9df6a9d907e99d17cf1cc962
Disha Navreti <dishanavreti09@gmail.com>
2026-04-23 04:11:59 +0000

    comparision

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,5 @@
+let con = 2+2;
+con = 9+9;
+// console.log(con);
+
+console.log(+"true");
---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,7 @@
+// console.log("2">1);
+// console.log(null>0);
+
+console.log(A==65);
+
+
+// === strictly check(check data type)
