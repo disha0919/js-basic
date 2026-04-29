6eeee4f349e9f4ecbc3aafc77a1c2ecf9c70508a
Disha Navreti <dishanavreti09@gmail.com>
2026-04-29 03:16:23 +0000

    math and num

---
+++ 01_basics/06nums_and_math.js
@@ -0,0 +1,34 @@
+// const score =400;
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 1123.8996;
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 100000000;
+// console.log(hundreds.toLocaleString('en-IN'));//indian styles ke hisab se (,)
+
+//++++++++++++++++ Maths +++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));// absolute me only (-) values (+) ho jati hai
+
+// console.log(Math.round(4.3));// round will round off the values and .5 se chhta hai to 4 angar bada to 5
+
+// console.log(Math.min(4,3,6,2));//For minimum value in array
+
+// console.log(Math.random());//ganerate random value 0 to 1
+// console.log(Math.floor((Math.random()*10)+1));//Ganerate random decimal values from 1 to 10 and if we did round then it will roundoff the value
+
+const min = 10;
+const max =20;
+console.log(Math.floor(Math.random() * (max - min + 1)) +10);//values from 10 to 20
+ 
+
+
+
